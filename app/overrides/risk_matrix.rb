67909abdcd815e7ff64ee5311220fe9044b150cb
anassdks <anass.dks@gmail.com>
2022-01-12 12:34:49 +0100

    set plugin version to 0.1.1

---
+++ app/overrides/risk_matrix.rb
@@ -0,0 +1,7 @@
+Deface::Override.new(
+  virtual_path: 'easy_risks/_matrix',
+  replace: 'table.risk-matrix',
+  template: 'cda_risks/matrix',
+  original: 'ed1a69924ce485186ebb67cbce2aa0f60ca11ad7',
+  method_access: false
+)
