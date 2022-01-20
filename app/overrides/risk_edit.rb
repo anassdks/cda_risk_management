bcd76596b7e2784fd8817291c1f72e8ab5365894
anassdks <anass.dks@gmail.com>
2022-01-20 07:50:33 +0100

    set plugin version to 0.1.8

--- app/overrides/risk_edit.rb
+++ app/overrides/risk_edit.rb
@@ -1,6 +1,11 @@
-Deface::Override.new(
-  virtual_path: 'easy_page_modules/others/_easy_risks_edit',
-  replace: "erb[loud]:contains('_compare_to_previous_period_paragraph')",
-  template: 'cda_risks/easy_risks_edit',
-  method_access: false
-)
+# if !@epzm.nil?
+#   @div = 'div#module_inside_modal_' + @epzm.uuid + '_compare_to_previous_period_paragraph'
+# else 
+#   @div = ''
+# end
+# Deface::Override.new(
+#   virtual_path: 'easy_page_modules/others/_easy_risks_edit',
+#   replace: 'div#module_inside_modal_' + @epzm.uuid + '_compare_to_previous_period_paragraph',
+#   template: 'cda_risks/easy_risks_edit',
+#   method_access: false
+# )
